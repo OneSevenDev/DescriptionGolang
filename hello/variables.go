190be7eac3b0081e7937087231cb712f11c20e26
manuelguarniz <cruzemg95@gmail.com>
2017-01-01 04:17:21 -0500

    detallado de variables

--- hello/variables.go
+++ hello/variables.go
@@ -4,11 +4,28 @@ import (
 	"fmt"
 )
 
+var barra = "código de barras"
+
+// barra := "código de barras" -> no permite, solo el := es valido dentro de funciones
+
 func main() {
 
+	//Varibles
+	_number := 17             //enteros
+	_name := "Manuel Guarniz" //cadena
+
+	var (
+		bol      = false
+		integer  = 21
+		textShow = "Texto de muestra"
+	)
+
+	fmt.Println(_number, _name, "\n ========================= \n", bol, integer, textShow)
+
+	//Ejemplos de variables
 	var number int
 	number = 25
-	fmt.Println(number)
+	fmt.Println("========================= \n", number)
 	number = 40
 	fmt.Println(number)
 
@@ -24,4 +41,12 @@ func main() {
 	fmt.Println(name, lastName)
 	name, lastName = lastName, name
 	fmt.Println(name, lastName)
+
+	//scope
+	//var barra = "código de barras" -> se cambio a variable global
+	fmt.Println("Al reverso del producto esta el " + barra)
+}
+
+func printText() {
+	fmt.Println("Funciona adicional: Al reverso del producto esta el " + barra)
 }
