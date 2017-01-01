fc9f62abbccaace92703fa272ba2dac0c37b516a
manuelguarniz <cruzemg95@gmail.com>
2017-01-01 12:06:59 -0500

    operadores matematicos y comparadores

---
+++ hello/operadores.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 49
+	b := 7
+
+	fmt.Println("\t Operadores \n", "****************************************")
+	fmt.Println(a, "\t + \t", b, "\t = \t", a+b)
+	fmt.Println(a, "\t - \t", b, "\t = \t", a-b)
+	fmt.Println(a, "\t * \t", b, "\t = \t", a*b)
+	fmt.Println(a, "\t / \t", b, "\t = \t", a/b)
+	fmt.Println(a, "\t % \t", b, "\t = \t", a%b)
+
+	c := 7
+	fmt.Println("\t Comparadores \n", "****************************************")
+	fmt.Println(a, "\t == \t", b, "\t ->\t ", a == b)
+	fmt.Println(b, "\t == \t", c, "\t ->\t ", b == c)
+	fmt.Println(a, "\t != \t", b, "\t ->\t ", a != b)
+	fmt.Println(c, "\t != \t", b, "\t ->\t ", c != b)
+	fmt.Println(a, "\t < \t", b, "\t ->\t ", a < b)
+	fmt.Println(a, "\t <= \t", b, "\t ->\t ", a <= b)
+	fmt.Println(c, "\t <= \t", b, "\t ->\t ", c <= b)
+	fmt.Println(a, "\t > \t", b, "\t ->\t ", a > b)
+	fmt.Println(a, "\t >= \t", b, "\t ->\t ", a >= b)
+}
